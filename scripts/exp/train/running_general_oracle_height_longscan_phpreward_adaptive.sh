cd04267ab199d2a57a14e2a7675bcc6037c97a5e
jason-wjs <jason-w@sjtu.edu.cn>
2026-05-31 14:53:30 +0800

    chore: tune general experiment env counts

--- scripts/exp/train/running_general_oracle_height_longscan_phpreward_adaptive.sh
+++ scripts/exp/train/running_general_oracle_height_longscan_phpreward_adaptive.sh
@@ -9,11 +9,11 @@ TASK="${TASK:-TT-Tracking-TerrainOracleHeightLongScanPhpRewardGeneral-Unitree-G1
 PAIR_DATASET="${PAIR_DATASET:-/home/humanoid/Downloads/Data/parc_initial_aug_g1/pair_dataset_running.jsonl}"
 DATASET_VALIDATE="${DATASET_VALIDATE:-fast}"
 EXPERIMENT_NAME="${EXPERIMENT_NAME:-tt_general_pair_dataset_running}"
-RUN_NAME="${RUN_NAME:-running_general_g1_oracle_height_longscan_phpreward_full_n16384_adaptive}"
+RUN_NAME="${RUN_NAME:-running_general_g1_oracle_height_longscan_phpreward_full_n8192_adaptive}"
 SAMPLING_MODE="${SAMPLING_MODE:-adaptive}"
 PAIR_SAMPLER_MODE="${PAIR_SAMPLER_MODE:-independent}"
 MAX_PAIRS="${MAX_PAIRS:-}"
-NUM_ENVS="${NUM_ENVS:-16384}"
+NUM_ENVS="${NUM_ENVS:-8192}"
 MAX_ITERATIONS="${MAX_ITERATIONS:-50000}"
 
 tt_train_general_pair_dataset_exp "$@"
